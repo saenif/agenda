3c205cc7bebeb3ef7a6281fe4107bc7811be14e2
Muresan Fineas <muresan.it@gmail.com>
2019-01-28 20:33:46 +0200

    feat(core): add router features
    - Create login module
    - Create contacts module
    - Create login component
    - Create contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ContactComponent} from './contacts/contact/contact.component';
+import {LoginComponent} from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contacts', component: ContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
